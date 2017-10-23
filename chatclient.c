de4b2ce846fb267ebb2493a2ea337b6dcd5157cd
Mert Anıl Gören <mrtanilgrn@gmail.com>
2017-10-23 18:04:24 +0300

    Add files via upload
    It works now :D Alpha testing will be done after the Server "It works now :D " update

--- chatclient.c
+++ chatclient.c
@@ -21,12 +21,30 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <poll.h>
+#include <fcntl.h>
 
 #define MAXCHAR 140 // Maximum character to transfer
 
 
 int errcheck; // glob error checking variable
 
+
+int socket_nonblock(int temp)
+{
+	int flags, s;
+
+	flags = fcntl(temp, F_GETFL, 0); // get fd to set non blocking
+	if (flags == -1) {
+		perror("server : fcntl");
+		return -1;
+	}
+	s = fcntl(temp, F_SETFL, flags | O_NONBLOCK);
+	if (s == -1) {
+		perror("server : fcntl");
+		return -1;
+	}
+	return 0;
+}
 int connecttoServer(char **argv) // Connects server and client
 {
 	struct addrinfo *p, *info, hints;
@@ -88,35 +106,63 @@ int main(int argc, char **argv)
 	struct pollfd mypoll = {STDIN_FILENO, POLLIN | POLLPRI };
 	int soc;
 	int running = 1;
-	char *msg;
+	char *rcvmsg, *sndmsg;
 
-	msg = calloc(MAXCHAR, sizeof(char));
-	if (msg == NULL) {
-		fprintf(stderr, "msg : calloc ");
+	rcvmsg = calloc(MAXCHAR, sizeof(char));
+	if (rcvmsg == NULL) {
+		fprintf(stderr, "rcvmsg : calloc ");
 		exit(0);
 	}
 
+
+
+
 	soc = connecttoServer(argv); // connect server
+	errcheck = socket_nonblock(soc);
 
 
 	while(running) { // event-client loop
 
-		if (poll(&mypoll, 1, 50)) {
-
-			printf("Enter > ");
-			fgets(msg, sizeof(msg), stdin);
-			if (msg == NULL) {
+		if (poll(&mypoll, 1, 50) > 0) {
+			sndmsg = calloc(MAXCHAR, sizeof(char));
+			if (sndmsg == NULL) {
+				fprintf(stderr, "sndmsg : calloc ");
+				exit(0);
+			}
+			fgets(sndmsg, MAXCHAR, stdin);
+			if (sndmsg == NULL) {
 				fprintf(stderr, "failed to load msg string");
-				continue;
+
 			}// end string if
-			send(soc, msg, sizeof(msg), 0);
-
-
-		}// end stdin if
-
-		recv(soc, msg, sizeof(msg), 0);
-		printf("%s\n", msg);
-
+			errcheck = send(soc, sndmsg, MAXCHAR, 0);
+			 if (errcheck < 0) {
+				 if(errno != EWOULDBLOCK ||
+					 errno != EAGAIN) {
+					 perror("send : ");
+				 }// if end
+					 continue;
+			 }// if end
+		}// end stdin send if
+		if(poll(&mypoll, soc, 50) > 0) {
+			errcheck = recv(soc, rcvmsg, MAXCHAR, 0);
+			if (errcheck < 0) {
+				if (errno != EAGAIN ||
+					errno != EWOULDBLOCK) {
+					perror("recv : ");
+				}
+				continue;
+			}
+			if (errcheck == 0) {
+				puts("Server Disconnected");
+				exit(0);
+			}
+			 if(rcvmsg == NULL) {
+				 continue;
+			 }
+			 else {
+				 printf("%s\n", rcvmsg);
+			 }
+		}
 	}// end while
 
 
